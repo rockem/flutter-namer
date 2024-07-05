b2a5587fa72eab3013c37c21d13a04700edbce5f
Eli Segal <eli.segal@dynamicyield.com>
2024-07-05 12:20:35 +0300

    Added pre-commit with flutter analyzing

--- test/generator_test.dart
+++ test/generator_test.dart
@@ -7,14 +7,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:namer_app/app.dart';
 import 'package:namer_app/generator.dart';
-
-import 'package:namer_app/main.dart';
 
 void main() {
   testWidgets('Generate random word', (WidgetTester tester) async {
-    final BuildContext context = tester.element(find.byType(Container));
     // Build our app and trigger a frame.
     await tester.pumpWidget(GeneratorPage());
 
